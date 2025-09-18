ca9bf725a21debd02f1bef9e349d914673d9f59f
abdennour siouane <31319771+aabdennour@users.noreply.github.com>
2025-09-19 01:45:48 +0200

    Create EmailServerVPS.sh

---
+++ EmailServerVPS.sh
@@ -0,0 +1,15 @@
+****************************************************
+  Debian 11, 12
+  Ubuntu 22.04, 24.04 LTS
+-----------------------------
+wget https://raw.githubusercontent.com/hestiacp/hestiacp/release/install/hst-install.sh
+apt-get update && apt-get install ca-certificates
+--------------------
+bash hst-install.sh --port '2096' --hostname 'panel.yoursite.com'
+confi dns in your dommaine add A record and host : panel with ip vps and save
+--------------------
+save username and password for your panel;
+username: tttttt
+password: #1hKhkslfdjf.*-/
+-----------------
+
